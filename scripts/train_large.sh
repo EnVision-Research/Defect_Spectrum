ce46c5a5013ada8ed203b8a0fbd4f7a49a2b118b
zhifeichen097 <35278797+zhifeichen097@users.noreply.github.com>
2024-07-03 12:26:17 +0800

    Update train_large.sh

--- scripts/train_large.sh
+++ scripts/train_large.sh
@@ -5,6 +5,6 @@ python -m torch.distributed.launch \
 --master_port=29513 \
 train.py \
 --work_dir /home/zfchen/working/diff_aug/COTTON_dataset/large_recep \
---config /home/zfchen/working/diff_aug/config/small_recep.yml \
+--config /home/zfchen/working/diff_aug/config/large_recep.yml \
 --seperate_channel_loss 0 \
 --num_defect 2
